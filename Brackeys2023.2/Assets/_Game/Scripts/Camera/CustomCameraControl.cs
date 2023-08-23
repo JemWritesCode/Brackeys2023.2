2b067e71728cdc23adfac5193e12edc32b4a5878
TheJadePhoenix <mattgmez98@gmail.com>
2023-08-23 09:27:21 -0400

    Created new PlayerTestPackage for combat
    Added prefab utilizing Skill framework to test combat

--- Brackeys2023.2/Assets/_Game/Scripts/Camera/CustomCameraControl.cs
+++ Brackeys2023.2/Assets/_Game/Scripts/Camera/CustomCameraControl.cs
@@ -3,57 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-[RequireComponent(typeof(CinemachineFreeLook))]
-public class CustomCameraControl : MonoBehaviour
+namespace _Game
 {
-    private CinemachineFreeLook freeLookCamera;
-    private bool canRotate = false;
-
-    private void Awake()
+    [RequireComponent(typeof(CinemachineFreeLook))]
+    public class CustomCameraControl : MonoBehaviour
     {
-        freeLookCamera = GetComponent<CinemachineFreeLook>();
-        if (freeLookCamera)
-        {
-            CinemachineCore.GetInputAxis = HandleAxisInputOverride;
-        }
-    }
+        protected CinemachineFreeLook freeLookCamera;
+        protected bool canRotate = false;
 
-    private float HandleAxisInputOverride(string axisName)
-    {
-        if (axisName == "Mouse X")
+        protected virtual void Awake()
         {
-            if (canRotate)
-                return Input.GetAxis("Mouse X");
-            else
-                return 0f;
-        }
-        else if (axisName == "Mouse Y")
-        {
-            if (canRotate)
-                return Input.GetAxis("Mouse Y");
-            else
-                return 0f;
+            freeLookCamera = GetComponent<CinemachineFreeLook>();
+            if (freeLookCamera)
+            {
+                CinemachineCore.GetInputAxis = HandleAxisInputOverride;
+            }
         }
 
-        return Input.GetAxis(axisName); // For other axes, return default value
-    }
-
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(1))
+        protected virtual float HandleAxisInputOverride(string axisName)
         {
-            canRotate = true;
+            if (axisName == "Mouse X")
+            {
+                if (canRotate)
+                    return Input.GetAxis("Mouse X");
+                else
+                    return 0f;
+            }
+            else if (axisName == "Mouse Y")
+            {
+                if (canRotate)
+                    return Input.GetAxis("Mouse Y");
+                else
+                    return 0f;
+            }
+
+            return Input.GetAxis(axisName); // For other axes, return default value
         }
 
-        if (Input.GetMouseButtonUp(1))
+        protected virtual void Update()
         {
-            canRotate = false;
-        }
-    }
+            if (Input.GetMouseButtonDown(1))
+            {
+                canRotate = true;
+            }
 
-    private void OnDestroy()
-    {
-        // Reset the delegate to avoid potential problems elsewhere
-        CinemachineCore.GetInputAxis = Input.GetAxis;
+            if (Input.GetMouseButtonUp(1))
+            {
+                canRotate = false;
+            }
+        }
+
+        protected virtual void OnDestroy()
+        {
+            // Reset the delegate to avoid potential problems elsewhere
+            CinemachineCore.GetInputAxis = Input.GetAxis;
+        }
     }
 }
