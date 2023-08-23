0f7906bee0f831ce41095e76c193ad061cf5e255
J Ricardo San <jricardosanchezibanez@gmail.com>
2023-08-23 02:33:03 +0200

    Added Melee and Take animations
    Added different Melee animations (Punch, Roll, Punish and Slam) as well as Take action.
    The test script shows how to call the corresponding functions to trigger these animations.

--- Brackeys2023.2/Assets/_Animation/Scripts/humanoidAnimationStateController.cs
+++ Brackeys2023.2/Assets/_Animation/Scripts/humanoidAnimationStateController.cs
@@ -7,6 +7,32 @@ namespace Animation
     public class humanoidAnimationStateController : MonoBehaviour
     {
 
+        // Internal class parameters
+        private Animator animator;
+        private int velFbwHash;
+        private int velLatHash;
+        private int takeItemHash;
+        private int meleePunchHash;
+        private int meleeRollHash;
+        private int meleePunishWindupHash;
+        private int meleePunishHitHash;
+        private int meleeSlamHash;
+
+        void Start()
+        {
+
+            animator = GetComponent<Animator>();
+            velFbwHash = Animator.StringToHash("vel_fbw");
+            velLatHash = Animator.StringToHash("vel_lat");
+            takeItemHash = Animator.StringToHash("action_take");
+            meleePunchHash = Animator.StringToHash("melee_punch");
+            meleeRollHash = Animator.StringToHash("melee_roll");
+            meleePunishWindupHash = Animator.StringToHash("melee_punish_windup");
+            meleePunishHitHash = Animator.StringToHash("melee_punish_hit");
+            meleeSlamHash = Animator.StringToHash("melee_slam");
+
+        }
+
         /* animateMotion
          * It indicates the State Controller the motion animation
          * to perform according to two input parameters:
@@ -22,9 +48,45 @@ namespace Animation
         public void animateMotion( float vel_fbw, float vel_lat)
         {
 
-            GetComponent<Animator>().SetFloat( "vel_fbw", vel_fbw );
-            GetComponent<Animator>().SetFloat( "vel_lat", vel_lat );
+            animator.SetFloat( velFbwHash, vel_fbw );
+            animator.SetFloat( velLatHash, vel_lat );
 
+        }
+
+        public void animateActionTakeItem()
+        {
+            animator.ResetTrigger( takeItemHash );
+            animator.SetTrigger( takeItemHash );
+        }
+
+        public void animateMeleePunch()
+        {
+            animator.ResetTrigger( meleePunchHash );
+            animator.SetTrigger( meleePunchHash );
+        }
+
+        public void animateMeleeRoll()
+        {
+            animator.ResetTrigger( meleeRollHash );
+            animator.SetTrigger( meleeRollHash );
+        }
+
+        public void animateMeleeStartPunish()
+        {
+            animator.ResetTrigger( meleePunishWindupHash );
+            animator.SetTrigger( meleePunishWindupHash );
+        }
+
+        public void animateMeleeInterruptPunish()
+        {
+            animator.ResetTrigger( meleePunishHitHash );
+            animator.SetTrigger( meleePunishHitHash );
+        }
+
+        public void animateMeleeSlam()
+        {
+            animator.ResetTrigger( meleeSlamHash );
+            animator.SetTrigger( meleeSlamHash );
         }
         
     }
--- Brackeys2023.2/Assets/_Animation/Scripts/unitTest_HumanoidAnimation.cs
+++ Brackeys2023.2/Assets/_Animation/Scripts/unitTest_HumanoidAnimation.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Using the Animation namespace created for this project
 using Animation;
 
 public class unitTest_HumanoidAnimation : MonoBehaviour
@@ -10,6 +11,12 @@ public class unitTest_HumanoidAnimation : MonoBehaviour
     humanoidAnimationStateController dummySC;
     [SerializeField] private float input_vel_fbw = 0.0f;
     [SerializeField] private float input_vel_lat = 0.0f;
+    [SerializeField] private bool input_take_item = false;
+    [SerializeField] private bool input_melee_punch = false;
+    [SerializeField] private bool input_melee_roll = false;
+    [SerializeField] private bool input_melee_punish = false;
+    [SerializeField] private bool input_melee_interrupt_punish = false;
+    [SerializeField] private bool input_melee_slam = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +29,51 @@ public class unitTest_HumanoidAnimation : MonoBehaviour
     {
 
         // Make the character execute a motion animation given user input
-        dummySC.animateMotion( input_vel_fbw, input_vel_lat );        
+        dummySC.animateMotion( input_vel_fbw, input_vel_lat );   
+
+        // Execute once taking the item
+        if (input_take_item)
+        {
+            dummySC.animateActionTakeItem();
+            input_take_item = false;
+        }
+
+        // MELEE Punch
+        if (input_melee_punch)
+        {
+            dummySC.animateMeleePunch();
+            input_melee_punch = false;
+        }
+
+        // MELEE Roll Dodge
+        if (input_melee_roll)
+        {
+            dummySC.animateMeleeRoll();
+            input_melee_roll = false;
+        }
+
+        // MELEE Start Punish
+        if (input_melee_punish)
+        {
+            dummySC.animateMeleeStartPunish();
+            input_melee_punish = false;
+        }
+
+        // MELEE Interrupt the Start Punish
+        if (input_melee_interrupt_punish)
+        {
+            dummySC.animateMeleeInterruptPunish();
+            input_melee_interrupt_punish = false;
+        }
+
+        // MELEE Slam
+        if (input_melee_slam)
+        {
+
+            dummySC.animateMeleeSlam();
+            input_melee_slam = false;
+
+        }
 
     }
 
